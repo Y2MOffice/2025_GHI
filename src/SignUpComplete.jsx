82824a1e88d27700c67d821c17be9e2bdfa67118
Subeen Chun <petitloutre@naver.com>
2025-01-29 11:28:44 +0900

    モバイル文字修正

--- src/SignUpComplete.jsx
+++ src/SignUpComplete.jsx
@@ -5,6 +5,7 @@ import {
   CssBaseline,
   ThemeProvider,
   createTheme,
+  useMediaQuery,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
@@ -39,10 +40,23 @@ const SignUpComplete = () => {
           height: "calc(100vh - 64px)", // 화면 전체 높이
         }}
       >
-        <Typography variant="h4" align="center" gutterBottom>
+        <Typography
+          variant="h4"
+          align="center"
+          gutterBottom
+          sx={{
+            fontSize: {
+              xs: "1.5rem", // 모바일 화면 (xs)에서는 글씨 크기를 작게
+              sm: "2rem", // 작은 화면 (sm)에서는 중간 크기
+              md: "2.5rem", // 일반 화면에서는 기본 크기
+            },
+          }}
+        >
           🎉おめでとう！🎉
           <br />
-          これからネットフリックスで楽しい時間を過ごしてください。
+          これからネットフリックスで楽しい
+          <br />
+          時間を過ごしてください。
         </Typography>
         <Button
           variant="contained"
